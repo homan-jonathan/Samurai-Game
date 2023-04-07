dbffc261fbf58fce704b45b6de96d9f8c0e9fcf0
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2023-04-07 12:37:54 -0400

    Added enemy detection radius and split movement into 2 scripts

---
+++ Assets/Scripts/Enemy/EnemyMoveScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoveScript : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    int waypointIndx = 0;
+
+    EnemySightScript enemySightScript;
+    public Transform _playerTransform;
+
+    public bool isWalking = false;
+    public bool isRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(waypoints[waypointIndx].position);
+
+        enemySightScript = GetComponent<EnemySightScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ReachedDestinationOrGaveUp())
+        {
+            SetNewWaypoint();
+        }
+        if (enemySightScript.CanSeePlayer())
+        {
+            PursuePlayer();
+        }
+    }
+
+    void SetNewWaypoint()
+    {
+        waypointIndx = (waypointIndx + 1) % waypoints.Length;
+        agent.SetDestination(waypoints[waypointIndx].position);
+        isRunning = false;
+        isWalking = true;
+
+    }
+
+    void PursuePlayer()
+    {
+        agent.SetDestination(_playerTransform.position);
+        isRunning = true;
+        isWalking = false;
+    }
+
+    //from DataGreed/UnityNavMeshCheck.cs github
+    bool ReachedDestinationOrGaveUp()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
