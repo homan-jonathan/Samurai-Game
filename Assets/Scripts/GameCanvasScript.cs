abdda4567a38dcd3e3bbf7b901df1ddf252d0c21
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2023-04-11 13:20:18 -0400

    Fixed the missing zone triggers

--- Assets/Scripts/GameCanvasScript.cs
+++ Assets/Scripts/GameCanvasScript.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class GameCanvasScript : MonoBehaviour
 {
+    public Text timerText;
     public Text zoneText;
+
+    float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,8 @@ public class GameCanvasScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        timerText.text = "Time: " + timer;
     }
     public IEnumerator DisplayZoneText(string locationName)
     {
