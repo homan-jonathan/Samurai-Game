21604cbede98ffae3893312ffde6ea6087c6727f
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2023-04-11 23:13:22 -0400

    what i have so far

--- Assets/Scripts/Enemy/EnemySightScript.cs
+++ Assets/Scripts/Enemy/EnemySightScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySightScript : MonoBehaviour
 {
@@ -12,17 +13,25 @@ public class EnemySightScript : MonoBehaviour
     public float VIEW_DISTANCE = 1;
     public float PLAYER_CROUCHING_MULTIPLIER = .25f;
     public float PLAYER_RUNNING_MULTIPLIER = 1.5f;
-    public float PLAYER_DETECTION_RADIUS = 1.5f;
+    //public float PLAYER_DETECTION_RADIUS = 1.5f;
     public float PLAYER_SPOTTED_DURATION = 1f;
     public float RAYCAST_COOLDOWN_TIME = .1f;
 
+    SightIndicatorScript _sightIndicatorScript;
     public float _seenPlayerRecently = 0;
     float _timeSinceLastRayCast = 0;
-    bool _playerInPossibleViewRange = false;
+    //bool _playerInPossibleViewRange = false;
+    bool _playerCloseToGaurd = false;
+
+    public Ray ray = new Ray();
+
+    public Image warningImage;
+    public Color warningColor;
+    public Color spottedColor;
     // Start is called before the first frame update
     void Start()
     {
-
+        _sightIndicatorScript = GetComponentInChildren<SightIndicatorScript>();
     }
 
     // Update is called once per frame
@@ -32,34 +41,100 @@ public class EnemySightScript : MonoBehaviour
         {
             _seenPlayerRecently -= Time.deltaTime;
         }
+
     }
 
     public bool CanSeePlayer()
     {
-        _timeSinceLastRayCast -= Time.deltaTime; //Check if last raycast was recent, if it was than act like guard can still see player
-        if (_timeSinceLastRayCast > 0) {
+        /*_timeSinceLastRayCast -= Time.deltaTime; //Check if last raycast was recent, if it was than act like guard can still see player
+        if (_timeSinceLastRayCast > 0)
+        {
             return true;
-        }
+        }*/
 
-        if (!_playerInPossibleViewRange) { //check if in collider, minimize raycast need
+        if (!_sightIndicatorScript._playerInPossibleViewRange) { //check if in collider, minimize raycast need
             return false;
         }
 
         Vector3 positionInFrontofHead = _headTransform.position + transform.rotation * new Vector3(0, 0, .25f);
         Vector3 directionToPlayer = (_playerTransform.position - transform.position).normalized;
 
-        if (PlayerInViewDistance(positionInFrontofHead, directionToPlayer) || PlayerInCloseProximity()) { //If can see player
-            _timeSinceLastRayCast = RAYCAST_COOLDOWN_TIME;
+
+        //Updates Ray position
+        ray.origin = positionInFrontofHead;
+        ray.direction = directionToPlayer;
+
+
+
+        /*if (_seenPlayerRecently > 0)
+        {
+            warningImage.color = new Color(spottedColor.r, spottedColor.g, spottedColor.b);
+        }
+        else
+        {
+            //hit player but is unseeable to guard
+            warningImage.color = new Color(warningColor.r, warningColor.g, warningColor.b);
+        }*/
+
+
+        //Sees if it is hitting player
+        RaycastHit hit;
+        if (Vector3.Angle(transform.forward, directionToPlayer) < 90 &&
+            Physics.Raycast(ray, out hit, VIEW_DISTANCE * PLAYER_RUNNING_MULTIPLIER))
+        {
+            if (hit.collider.tag == Tag.player)
+            {
+                warningImage.enabled = true;
+                if (hit.distance <= CalculateViewDistance())
+                {
+                    //seen player
+                    warningImage.color = new Color(spottedColor.r, spottedColor.g, spottedColor.b);
+                }
+                else
+                {
+                    //hit player but is unseeable to guard
+                    warningImage.color = new Color(warningColor.r, warningColor.g, warningColor.b);
+                }
+
+
+                /*if (_seenPlayerRecently < 0)
+                {
+                    warningImage.enabled = false;
+                }*/
+            }
+        }
+
+        if (!PlayerInCloseProximity() && 
+            Vector3.Angle(transform.forward, directionToPlayer) < 90 &&
+            Physics.Raycast(ray, out hit, VIEW_DISTANCE * PLAYER_RUNNING_MULTIPLIER))
+        {
+            warningImage.enabled = false; 
+        }
+
+        if (PlayerInViewDistance(directionToPlayer) || PlayerInCloseProximity())
+        { //If can see player
+            //_timeSinceLastRayCast = RAYCAST_COOLDOWN_TIME;
             return true;
         }
+
+
 
         return false;
     }
 
-    bool PlayerInViewDistance(Vector3 positionInFrontofHead, Vector3 directionToPlayer)
+    bool PlayerInViewDistance(Vector3 directionToPlayer)
     {
         RaycastHit hit;
-        if (Vector3.Angle(transform.forward, directionToPlayer) < VIEW_ANGLE / 2 && 
+        if (Vector3.Angle(transform.forward, directionToPlayer) < VIEW_ANGLE / 2 &&
+            Physics.Raycast(ray, out hit, CalculateViewDistance()))
+        {
+            if (hit.collider.tag == Tag.player) //If ray hit player in the ViewDistance and proper angle
+            {
+                _seenPlayerRecently = PLAYER_SPOTTED_DURATION;
+                return true;
+            }
+        }
+        /*if (Vector3.Angle(transform.forward, directionToPlayer) < VIEW_ANGLE / 2 && 
             Physics.Raycast(positionInFrontofHead, directionToPlayer, out hit, VIEW_DISTANCE * CalculateViewDistance()))
         {
             if (hit.collider.tag == Tag.player)
@@ -67,7 +142,7 @@ public class EnemySightScript : MonoBehaviour
                 _seenPlayerRecently = PLAYER_SPOTTED_DURATION;
                 return true;
             }
-        }
+        }*/
 
         return false;
     }
@@ -84,7 +159,7 @@ public class EnemySightScript : MonoBehaviour
             viewDistanceMultiplier = PLAYER_RUNNING_MULTIPLIER;
         }
 
-        return viewDistanceMultiplier;
+        return viewDistanceMultiplier * VIEW_DISTANCE;
     }
 
     bool PlayerInCloseProximity() {
@@ -92,7 +167,7 @@ public class EnemySightScript : MonoBehaviour
             return false;
         }
 
-        RaycastHit[] hits = Physics.SphereCastAll(transform.position, PLAYER_DETECTION_RADIUS, Vector3.forward);
+        /*RaycastHit[] hits = Physics.SphereCastAll(transform.position, PLAYER_DETECTION_RADIUS, Vector3.forward);
         foreach (RaycastHit hit in hits)
         {
             if (hit.collider.tag == Tag.player)
@@ -100,20 +175,21 @@ public class EnemySightScript : MonoBehaviour
                 _seenPlayerRecently = PLAYER_SPOTTED_DURATION;
                 return true;
             }
-        }
+        }*/
 
-        return false;
+        return _playerCloseToGaurd;
     }
 
-    public float GetViewDistance() {
+    /*public float GetViewDistance() {
         return VIEW_DISTANCE * CalculateViewDistance();
-    }
+    }*/
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals(Tag.player))
         {
-            _playerInPossibleViewRange = true;
+            _playerCloseToGaurd = true;
+            //_playerInPossibleViewRange = true;
         }
     }
 
@@ -121,7 +197,8 @@ public class EnemySightScript : MonoBehaviour
     {
         if (other.gameObject.tag.Equals(Tag.player))
         {
-            _playerInPossibleViewRange = false;
+            _playerCloseToGaurd = false;
+            //_playerInPossibleViewRange = false;
         }
     }
 }
--- Assets/Scripts/Enemy/SightIndicatorScript.cs
+++ Assets/Scripts/Enemy/SightIndicatorScript.cs
@@ -9,34 +9,39 @@ public class SightIndicatorScript : MonoBehaviour
     public Color warningColor;
     public Color spottedColor;
     
-    SphereCollider sphereCollider;
-    EnemySightScript enemySightScript;
+    EnemySightScript _enemySightScript;
+    SphereCollider _sphereCollider;
+    public bool _playerInPossibleViewRange = false;
     // Start is called before the first frame update
     void Start()
     {
-        enemySightScript = GetComponent<EnemySightScript>();
-        sphereCollider = GetComponent<SphereCollider>();
+        _enemySightScript = GetComponentInParent<EnemySightScript>();
+        _sphereCollider = GetComponent<SphereCollider>();
+        _sphereCollider.radius = _enemySightScript.VIEW_DISTANCE * _enemySightScript.PLAYER_RUNNING_MULTIPLIER;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemySightScript._seenPlayerRecently > 0)
+        /*if (_enemySightScript._seenPlayerRecently > 0)
         {
             warningImage.color = new Color(spottedColor.r, spottedColor.g, spottedColor.b);
         }
         else
         {
             warningImage.color = new Color(warningColor.r, warningColor.g, warningColor.b);
-        }
-        sphereCollider.radius = enemySightScript.GetViewDistance();
+        }*/
+        
+        
+        //_sphereCollider.radius = _enemySightScript.GetViewDistance();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals(Tag.player))
         {
-            warningImage.enabled = true;
+            //warningImage.enabled = true;
+            _playerInPossibleViewRange = true;
         }
     }
 
@@ -44,7 +49,7 @@ public class SightIndicatorScript : MonoBehaviour
     {
         if (other.gameObject.tag.Equals(Tag.player))
         {
-            warningImage.enabled = false;
+            _playerInPossibleViewRange = false;
         }
     }
 }
