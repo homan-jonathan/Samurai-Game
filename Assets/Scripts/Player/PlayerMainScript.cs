b257c4b3b9d1da6479aef7c898276a2dffe32cff
homan-jonathan <homanjp20@gcc.edu>
2023-04-12 20:08:03 -0400

    working charge jump

--- Assets/Scripts/Player/PlayerMainScript.cs
+++ Assets/Scripts/Player/PlayerMainScript.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMainScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject _coinBag;
+    PlayerMoveScript _moveScript;
 
+    public GameObject _coinBag;
     public bool _hasCoins;
 
     public bool isDead = false;
+
+    public Slider _healthBar;
+    public Image _healthFilledImage;
+
     void Start()
     {
-        
+        _moveScript = GetComponent<PlayerMoveScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _healthBar.value = _moveScript.chargeJumpTimer;
     }
 
     public void pickupCoin()
