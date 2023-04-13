f4fe8c54c96e88fd37ca6450cbc1dc6d28b1c931
homan-jonathan <homanjp20@gcc.edu>
2023-04-12 21:53:50 -0400

    coin pickup logic

--- Assets/Scripts/Player/CoinPickupScript.cs
+++ Assets/Scripts/Player/CoinPickupScript.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Json;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinPickupScript : MonoBehaviour
 {
 
     public GameObject _player;
+    public GameObject _interactPopupText;
+    public Text _text;
+
     PlayerMainScript _playerScript;
 
     
@@ -14,21 +18,20 @@ public class CoinPickupScript : MonoBehaviour
     void Start()
     {
         _playerScript = _player.GetComponent<PlayerMainScript>();
+        _interactPopupText.SetActive(false);
+
+        _text = _interactPopupText.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-            
+        _text.text = "Press " + KeyBinding.interact() + " to interact";
     }
 
     private void OnTriggerStay(Collider other)
     {
-        /*if(Input.GetKeyDown(KeyCode.E)) 
-        {
-            _playerScript.pickupCoin();
-            Destroy(gameObject);
-        }*/
+        
         if (Input.GetKeyDown(KeyBinding.interact()))
         {
             _playerScript.pickupCoin();
@@ -37,5 +40,22 @@ public class CoinPickupScript : MonoBehaviour
             gameObject.SetActive(false);
         }
         
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == Tag.player)
+        {
+            _interactPopupText.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == Tag.player)
+        {
+            _interactPopupText.SetActive(false);
+        }
+
     }
 }
