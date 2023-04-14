6d076f9ebc65c3983eaafe8384914123c7a2c3b7
homan-jonathan <homanjp20@gcc.edu>
2023-04-14 18:57:44 -0400

    update from main

--- Assets/Scripts/Enemy/EnemyAttackScript.cs
+++ Assets/Scripts/Enemy/EnemyAttackScript.cs
@@ -7,6 +7,8 @@ public class EnemyAttackScript : MonoBehaviour
     EnemyAnimScript enemyAnimScript;
     public float SWING_DISTANCE = 1;
     public Transform playerTransform;
+    [Range(0,360)]
+    public float SWING_ANGLE = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,11 @@ public class EnemyAttackScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(playerTransform.position, transform.position) <= SWING_DISTANCE) {
+        if (Vector3.Distance(playerTransform.position, transform.position) <= SWING_DISTANCE &&
+            Vector3.Angle(transform.forward, playerTransform.position - transform.position) < SWING_ANGLE &&
+            !enemyAnimScript.AnimationIsPlaying(AnimationState.swingSword)
+            )
+        {
             enemyAnimScript.PlayAttackAnim();
         }
     }
