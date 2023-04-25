28cff44cac20d57c29ed681cbdc771862069e40c
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2023-04-24 21:18:34 -0400

    RestructureGuradsLayout

---
+++ Assets/Scripts/Enemy/WaypointMeleeGuard/WMEnemyAnimScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WMEnemyAnimScript : MonoBehaviour
+{
+    public GameObject PLAYER;
+    
+    Animator _anim;
+    WMEnemyMoveScript _enemyScript;
+    GuardSoundScript _guardSounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _enemyScript = GetComponent<WMEnemyMoveScript>();
+        _guardSounds = GetComponent<GuardSoundScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _anim.SetBool("isWalking", _enemyScript.IsWalking());
+        _anim.SetBool("isRunning", _enemyScript.IsRunning());
+    }
+
+    public void PlayAttackAnim() {
+        _anim.SetTrigger("swingSword");
+        _guardSounds.SwordSlashNoise();
+    }
+
+    //from edu4hd0
+    bool AnimatorIsPlaying()
+    {
+        return _anim.GetCurrentAnimatorStateInfo(0).length >
+               _anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    //from edu4hd0
+    public bool AnimationIsPlaying(string stateName)
+    {
+        return AnimatorIsPlaying() && _anim.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    public GameObject GetPlayerReference() {
+        return PLAYER;
+    }
+}
