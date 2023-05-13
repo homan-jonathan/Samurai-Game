0f1ab203a71a043ec8aaec8b646c477aa9d4171b
homan-jonathan <homanjp20@gcc.edu>
2023-05-13 14:41:27 -0400

    player raycasting to enemy check

--- Assets/Scripts/Player/PlayerRaycastScript.cs
+++ Assets/Scripts/Player/PlayerRaycastScript.cs
@@ -1,24 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class PlayerRaycastScript : MonoBehaviour
 {
     public LayerMask mask;
+    Transform _transform;
+    CharacterController _charCon;
  
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = transform;
+        _charCon = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out var hit, Mathf.Infinity, mask))
+        if (RaycastHitsEnemy())
+        {
+            print("facing enemy");
+        }
+    }
+
+    private bool RaycastHitsEnemy()
+    {
+        RaycastHit hit;
+
+        Vector3 startPoint = transform.position + (_charCon.center + new Vector3(0, 1f, 0));
+
+        if (Physics.SphereCast(startPoint, _charCon.height / 2, transform.forward, out hit, 10000))
         {
             var obj = hit.collider.gameObject;
 
-            Debug.Log($"looking at {obj.name}", this);
+            if (obj.tag == Tag.enemy)
+            {
+                return true;
+            }
         }
+        return false;
     }
 }
