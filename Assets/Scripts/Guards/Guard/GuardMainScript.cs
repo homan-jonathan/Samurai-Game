7fd6f21d2ad36c04cc85505dc43847abf48d6144
homan-jonathan <homanjp20@gcc.edu>
2023-05-14 21:13:18 -0400

    changing tracking

--- Assets/Scripts/Guards/Guard/GuardMainScript.cs
+++ Assets/Scripts/Guards/Guard/GuardMainScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GuardMainScript : MonoBehaviour
 {
     public GameObject PLAYER;
+    GuardShaderScript _shaderScript;
     //public XRayCapsuleScript _xRayCapsule;
 
     // Start is called before the first frame update
@@ -12,13 +13,25 @@ public class GuardMainScript : MonoBehaviour
     {
         //_xRayCapsule = GetComponentInChildren<XRayCapsuleScript>();
         //_xRayCapsule.gameObject.SetActive(false);
+        _shaderScript = GetComponentInChildren<GuardShaderScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "TagGuardCollider")
+        {
+            if (Input.GetKeyDown(KeyBinding.interact())){
+                _shaderScript._isTagged = true;
+                print("tag enemy");
+            }
+        }
+    }
+
     public GameObject GetPlayerReference()
     {
         return PLAYER;
--- Assets/Scripts/Player/PlayerMainScript.cs
+++ Assets/Scripts/Player/PlayerMainScript.cs
@@ -60,6 +60,7 @@ public class PlayerMainScript : MonoBehaviour
         }
     }
 
+
     public bool IsDead()
     {
         return _isDead;
