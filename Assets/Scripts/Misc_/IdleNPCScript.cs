68cbb9692c48071b169494c6b206e20d9b29fb48
homan-jonathan <homanjp20@gcc.edu>
2023-05-14 21:46:11 -0400

    NPC's no longer get tracking shader

--- Assets/Scripts/Misc_/IdleNPCScript.cs
+++ Assets/Scripts/Misc_/IdleNPCScript.cs
@@ -7,12 +7,17 @@ public class IdleNPCScript : MonoBehaviour
     Transform _transform;
     Transform _playerTransform;
     PlayerMoveScript _playerScript;
+    Shader basicShader;
+    SkinnedMeshRenderer _skinnedMeshRenderer;
     // Start is called before the first frame update
     void Start()
     {
         _transform = transform;
         _playerScript = FindObjectOfType<PlayerMoveScript>();
         _playerTransform = _playerScript.GetComponent<Transform>();
+
+        basicShader = Shader.Find("Standard");
+        _skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
     }
 
     // Update is called once per frame
@@ -20,5 +25,6 @@ public class IdleNPCScript : MonoBehaviour
     {
         _transform.LookAt(_playerTransform);
         _transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        _skinnedMeshRenderer.material.shader = basicShader;
     }
 }
